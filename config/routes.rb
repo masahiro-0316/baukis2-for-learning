d2fb320e95e5b4925f76ad8e5b3d4645a39f01a8
masahiro <masahiro@sefira.arcana>
2023-01-12 12:56:59 +0900

    ホスト名による制約の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :staff, path: "" do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
-    resource :session, only: [ :create, :destroy ]
-    resource :account, except: [ :new, :create, :destroy]
+  config = Rails.application.config.baukis2
+
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [ :create, :destroy ]
+      resource :account, except: [ :new, :create, :destroy]
+    end
   end
 
-  namespace :admin do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", ad: :session
-    delete "session" => "sessions#destroy"
-    resource :session, only: [ :create, :destroy ]
-    resources :staff_members
+  constraints host: config[:admin][:host] do
+    namespace :admin, path: config[:admin][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [ :create, :destroy ]
+      resources :staff_members
+    end
   end
 
   namespace :customer do
