b62ec6b078d26605df470fc498d5f68f9aa8ded6
masahiro <masahiro@sefira.arcana>
2023-01-29 10:43:13 +0900

    10-1-2 レコード表示の実装 #6
    職員登録情報のレコード表示の実装完了
    ロゴマークからTopページへのリンクも実装

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -1,2 +1,5 @@
-class Admin::StaffMembersController < ApplicationController
+class Admin::StaffMembersController < Admin::Base
+  def index
+    @staff_members = StaffMember.order(:family_name, :given_name)
+  end
 end
--- db/seeds/development/staff_members.rb
+++ db/seeds/development/staff_members.rb
@@ -8,14 +8,14 @@ StaffMember.create!(
   start_date: Date.today
 )
 
-family_name = %w{
+family_names = %w{
   佐藤:サトウ:sato
   鈴木:スズキ:suzuki
   高橋:タカハシ:takahashi
   田中:タナカ:tanaka
 }
 
-given_name = %w{
+given_names = %w{
   二郎:ジロウ:jiro
   三郎:サブロウ:saburo
   松子:マツコ:matsuko
@@ -24,18 +24,18 @@ given_name = %w{
 }
 
 20.times do |n|
-  fn = family_name[n % 4].split(":")
-  gn = given_name[n % 5].split(":")
+  fn = family_names[n % 4].split(":")
+  gn = given_names[n % 5].split(":")
 
   StaffMember.create!(
-    email: "#{fn[2]}.#{gn(2)@example.com}"
+    email: "#{fn[2]}.#{gn[2]}@example.com",
     family_name: fn[0],
     given_name: gn[0], 
     family_name_kana: fn[1],
     given_name_kana: gn[1],
     password: "password",
     start_date: (100 -n).days.ago.to_date,
-    end_date: n == 0 ? Date.today : nill,
+    end_date: n == 0 ? Date.today : nil,
     suspended: n == 1
   )
 end
