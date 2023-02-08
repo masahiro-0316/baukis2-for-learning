c70cdf6e3786efadab81921e965a73bad7a64fe0
masahiro <masahiro@sefira.arcana>
2023-02-08 20:39:03 +0900

    構文ミス修正 #9

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -30,10 +30,10 @@ class Admin::Base < ApplicationController
 
   private def check_timeout
     if current_administrator
-      if session[:admin_last_access_time] = TIMEOUT.ago
+      if session[:admin_last_access_time] >= TIMEOUT.ago
         session[:admin_last_access_time] = Time.current
       else
-        session.delete(:administrator_ud)
+        session.delete(:administrator_id)
         flash.alert = "セッションがタイムアウトしました"
         redirect_to :admin_login
       end
